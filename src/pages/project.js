96d661249018e29877f9b618c6e4df9d95042bd4
Bhuwan <bhuwandutt2@gmail.com>
2022-12-24 18:17:09 -0500

    Testing Link component

---
+++ src/pages/project.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+// Step 2: Define your component
+const ProjectPage = () => {
+  return (
+    <main>
+      <h1>My Projects</h1>
+        <Link to="/projects">Github</Link>
+      <p>These are the projects I have done</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+// Step 3: Export your component
+export default ProjectPage
