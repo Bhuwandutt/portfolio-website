7e7caf54f10b8e8b0e3708064403baf6cb0d544e
Bhuwan <bhuwandutt2@gmail.com>
2022-12-24 20:47:17 -0500

    Styling and shit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,28 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import {container,
+    navLinks, navLinkText, heading, navLinkItem
+        } from './layout.module.css'
+
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <div className={container}>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/aboutme">About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+              <Link to="/" className={navLinkText} >Home </Link>
+          </li>
+          <li className={navLinkItem}>
+              <Link to="/aboutme" className={navLinkText}>About</Link>
+          </li>
+          <li className={navLinkItem}>
+              <Link to= "/project" className={navLinkText}>Projects</Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}> {pageTitle}</h1>
         {children}
       </main>
     </div>
