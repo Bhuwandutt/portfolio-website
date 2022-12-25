e2fdd12466890d67ff1967f8dec609fc9f9cf61c
Bhuwan <bhuwandutt2@gmail.com>
2022-12-24 22:33:04 -0500

    Installing Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@ module.exports = {
     title: `PortfolioWebsite`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+      `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-styled-components`,
+  ] // I AM NEW],
 }
