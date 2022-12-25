4d122fa1ad949b33771da64c665cc39cb73bc3b0
Bhuwan <bhuwandutt2@gmail.com>
2022-12-24 21:54:07 -0500

    Styling and shit

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,14 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from "../components/layout";
 // Step 2: Define your component
 const ProjectPage = () => {
   return (
-    <main>
-      <h1>My Projects</h1>
-        <Link to="/">Home</Link>
-      <p>These are the projects I have done</p>
-    </main>
+    <Layout pageTitle="About Me">
+        <p>These are the projects I have done</p>
+    </Layout>
+
   )
 }
 
